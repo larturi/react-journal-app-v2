0794b506a5a9534f4e246b7fb334a3a14eb07bc8
Leandro Arturi <lea.arturi@gmail.com>
2022-12-03 10:31:34 -0300

    Register Page

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,3 +1,66 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from '@mui/icons-material'
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import AuthLayout from '../layout/AuthLayout'
+
 export default function RegisterPage() {
-  return <div>RegisterPage</div>
+  return (
+    <AuthLayout title="Regirsto">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre Completo"
+              type="text"
+              placeholder="Tu nombre"
+              fullWidth
+              inputProps={{
+                form: {
+                  autoComplete: 'off',
+                },
+              }}
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="Correo Electrónico"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Tu Contraseña"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}> Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  )
 }
