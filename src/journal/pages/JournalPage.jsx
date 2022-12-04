ae86c93284d1695cda6096cf7c8f6252da8ed3e5
Leandro Arturi <lea.arturi@gmail.com>
2022-12-04 18:01:44 -0300

    FloatingButton

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,27 @@
+import { IconButton } from '@mui/material'
+import { AddOutlined } from '@mui/icons-material'
 import { JournalLayout } from '../layout'
-import { NoteView } from '../views'
+import { NothingSelectedView } from '../views'
 
 const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
