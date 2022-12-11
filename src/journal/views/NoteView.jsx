a7e9474dec6f5a19f8f1a7f7cb36a7112dabed45
Leandro Arturi <lea.arturi@gmail.com>
2022-12-11 11:17:09 -0300

    Animations

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -5,6 +5,7 @@ import ImageGallery from '../components/ImageGallery'
 export const NoteView = () => {
   return (
     <Grid
+      className="animate__animated animate__fadeIn animate__faster"
       container
       direction="row"
       justifyContent="space-between"
